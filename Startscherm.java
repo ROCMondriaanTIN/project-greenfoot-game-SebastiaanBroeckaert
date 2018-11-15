8ff5de68e4ef79e2685fed1a275c4488399cb1b7
SebastiaanBroeckaert <44869880+SebastiaanBroeckaert@users.noreply.github.com>
2018-11-15 11:02:22 +0100

    Startscherm gemaakt en een hele boel afbeeldingen toegevoegdt.

---
+++ Startscherm.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class startscherm here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Startscherm extends World
+{
+    /**
+     * Constructor for objects of class startscherm.
+     * 
+     */
+    public Startscherm()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1);
+        
+        prepare();
+    }
+    
+    /**
+     * Maakt de startscherm mooi.
+     */
+    private void prepare()
+    {
+        StartschermAfbeelding startscherm = new StartschermAfbeelding();
+        addObject(startscherm, 500, 400);
+    }
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("spacebar"))
+        {
+            Greenfoot.setWorld(new SelectLevel());
+        }
+    }
+}
