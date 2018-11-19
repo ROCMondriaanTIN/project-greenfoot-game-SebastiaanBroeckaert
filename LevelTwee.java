2e574a5f3de85daa3aa07aab0508ef60faaf7cfd
SebastiaanBroeckaert <44869880+SebastiaanBroeckaert@users.noreply.github.com>
2018-11-19 09:38:48 +0100

    Levels iets groter gemaakt.

--- LevelTwee.java
+++ LevelTwee.java
@@ -16,6 +16,6 @@ public class LevelTwee extends World
     public LevelTwee()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1000, 800, 1); 
     }
 }
